f40c86ebdd7e149ef43d1de1ba5ff144b5e4a550
k1nderko <107281239+k1nderko@users.noreply.github.com>
2023-12-05 19:28:11 +0200

    02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,23 +1,17 @@
-
-import Vimeo from '@vimeo/player';
+import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
-const playerIframe = document.getElementById('vimeo-player');
-
-const player = new Vimeo(playerIframe);
-
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
 const storageKey = "videoplayer-current-time";
 
-const saveTimeToStorage = throttle(async () => {
-  const currentTime = await player.getCurrentTime();
-  localStorage.setItem(storageKey, currentTime.toString());
-}, 1000); 
-
-player.on('timeupdate', saveTimeToStorage);
-
 const storedTime = localStorage.getItem(storageKey);
-
 if (storedTime) {
-  player.setCurrentTime(parseFloat(storedTime));
+  player.setCurrentTime(storedTime);
 }
 
+player.on('timeupdate', throttle(onTimeUpdate, 1000));
+
+function onTimeUpdate({ seconds }) {
+  localStorage.setItem(storageKey, seconds);
+}
