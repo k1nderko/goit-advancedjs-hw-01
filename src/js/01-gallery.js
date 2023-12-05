5acc4a7828d9847b6687bb779b242bee73e123d9
k1nderko <107281239+k1nderko@users.noreply.github.com>
2023-12-05 16:45:08 +0200

    2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,37 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 console.log(galleryItems);
+
+const refs = {
+  listGallery: document.querySelector('.gallery'),
+};
+
+// refs.listGallery.addEventListener('click', selectImg);
+
+function createMarkup(arr) {
+  return arr
+    .map(({ preview, original, description }) => `
+      <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            // data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </li>`
+    )
+    .join('');
+}
+
+refs.listGallery.innerHTML = createMarkup(galleryItems);
+
+let gallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
