b2b7ad3b423f1c4efd3f02a0afc13f60191f3e52
k1nderko <107281239+k1nderko@users.noreply.github.com>
2023-12-05 19:39:18 +0200

    03

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,37 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const emailInput = form.querySelector('[name="email"]');
+const messageInput = form.querySelector('[name="message"]');
+const storageKey = "feedback-form-state";
+
+const saveStateToStorage = throttle(() => {
+  const formState = {
+    email: emailInput.value,
+    message: messageInput.value
+  };
+  localStorage.setItem(storageKey, JSON.stringify(formState));
+}, 500);
+
+const fillFormFromStorage = () => {
+  const storedState = localStorage.getItem(storageKey);
+  if (storedState) {
+    const formState = JSON.parse(storedState);
+    emailInput.value = formState.email;
+    messageInput.value = formState.message;
+  }
+};
+
+form.addEventListener('input', saveStateToStorage);
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  console.log({
+    email: emailInput.value,
+    message: messageInput.value
+  });
+  localStorage.removeItem(storageKey);
+  emailInput.value = "";
+  messageInput.value = "";
+});
+
+fillFormFromStorage();
